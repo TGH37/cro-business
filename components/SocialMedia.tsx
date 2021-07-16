e402829d7b4a7e79582d2271a8181727274736f4
Tom Hayton <t.hayton37@gmail.com>
2021-07-16 17:46:09 +0100

    Updated LinkeIn Link

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -10,7 +10,7 @@ function SocialMedia(props: Props) {
 
   return (
     <div className={styles.socialMediaContainer}>
-      <Link href="https://www.linkedin.com/feed/"><a target="_blank"><FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" className={styles.linkedin}/></a></Link>
+      <Link href="https://www.linkedin.com/company/74572571/"><a target="_blank"><FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" className={styles.linkedin}/></a></Link>
       <Link href="https://www.facebook.com/Convergence-CRO-100320065667890"><a target="_blank"><FontAwesomeIcon icon={["fab", "facebook"]} size="2x"className={styles.facebook}/></a></Link>
     </div>
   );
